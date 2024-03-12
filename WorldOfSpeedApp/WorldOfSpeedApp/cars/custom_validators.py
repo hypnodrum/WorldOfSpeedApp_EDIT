701c72b5b299014312622f1b014a48670c442a95
Ivan Vasilev <144450863+hypnodrum@users.noreply.github.com>
2024-03-12 11:21:24 +0200

    Update custom_validators.py

--- WorldOfSpeedApp/WorldOfSpeedApp/cars/custom_validators.py
+++ WorldOfSpeedApp/WorldOfSpeedApp/cars/custom_validators.py
@@ -3,4 +3,4 @@ from django.core.exceptions import ValidationError
 
 def car_year_validator(year):
     if year < 1950 or year > 2030:
-        raise ValidationError("Year must be between 1999 and 2030!")
+        raise ValidationError("Year must be between 1950 and 2030!")
